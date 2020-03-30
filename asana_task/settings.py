18f55eaa3eace4acc855ed0d4eca4802038ef095
Artush Ghazaryan <51846629+artush96@users.noreply.github.com>
2020-03-30 22:48:26 +0400

    Update settings.py

--- asana_task/settings.py
+++ asana_task/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+2b63l*!$a06_9o^go@59l4zh&+n^1&5*_&)$kc#s2sb7(t=nw'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
